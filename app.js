837f04f7c903539d74bd570bf3ce768c0ea858f4
Subhra264 <chakrabortysubhradeep556@gmail.com>
2022-05-30 00:29:47 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const config = require("./config.json");
+
+//== connect to database
+const mongoURI =
+  config.MONGODB_URI || "mongodb://localhost:27017" + "/newsFeed";
+
+let mongoose = require("mongoose");
+const newsArticleModel = require("./model");
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("connected to database"));
+
+const onePageArticleCount = 10;
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello world!");
+});
+
+// your code here!
+
+app.get("/newFeeds", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || onePageArticleCount;
+    const offset = parseInt(req.query.offset) || 0;
+    const articles = await newsArticleModel.find().skip(offset).limit(limit);
+    res.status(200).send(articles);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// ==end==
+
+module.exports = { app, db };
